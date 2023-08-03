6083a8a67c35f8654bc4933f9f9c595c5fc39e57
Mike Tilburg <mtilburg@adobe.com>
2023-08-03 20:59:18 +0200

    move to loadEager

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -69,6 +69,20 @@ export function decorateMain(main) {
   decorateBlocks(main);
 }
 
+const loadScript = (url, attrs) => {
+  const head = document.querySelector('head');
+  const script = document.createElement('script');
+  script.src = url;
+  if (attrs) {
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
+    for (const attr in attrs) {
+      script.setAttribute(attr, attrs[attr]);
+    }
+  }
+  head.append(script);
+  return script;
+};
+
 /**
  * Loads everything needed to get to LCP.
  * @param {Element} doc The container element
@@ -81,6 +95,15 @@ async function loadEager(doc) {
     decorateMain(main);
     document.body.classList.add('appear');
     await waitForLCP(LCP_BLOCKS);
+  }
+
+  // Load Launch properties (adobedtm)
+  if (window.location.host.startsWith('localhost')) {
+    await loadScript('https://assets.adobedtm.com/6a74768abd57/a692f024da9a/launch-6005424708d4-development.min.js');
+  } else if (window.location.host.endsWith('.page')) {
+    await loadScript('https://assets.adobedtm.com/6a74768abd57/a692f024da9a/launch-166628721e50-staging.min.js');
+  } else {
+    await loadScript('https://assets.adobedtm.com/6a74768abd57/a692f024da9a/launch-3ae9c8b61452.min.js');
   }
 
   try {
